10fe35d35c912f921e267e30247ef0bc57aeb2df
Marcos Valera <marvalpra1@alum.us.es>
2026-09-24 16:09:34 +0200

    feat: Añadir validación de edad y nivel físico para valores imposibles

--- src/viaje_ej3.py
+++ src/viaje_ej3.py
@@ -1,9 +1,15 @@
 edad = int(input("Introduce tu edad >>> "))
 
+while edad < 1 or edad > 99:
+    edad = int(input("Debes de introducir una edad entre 1 y 99 >>> "))
 if edad < 18:
     print("Debes ser mayor de edad.")
 else:
     nivel_fisico = int(input("Introduce tu nivel físico (1-10) >>> "))
+
+    while nivel_fisico < 0 or nivel_fisico > 10:
+        nivel_fisico = int(input("Debes introducir un número entre 1 y 10 >>> "))
+
     if nivel_fisico < 5:
         print("Debes estar en mejor forma.")
     else:
