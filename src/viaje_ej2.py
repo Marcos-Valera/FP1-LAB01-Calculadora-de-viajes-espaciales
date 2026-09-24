257e977dc2ffd94574e5018ffee86c67ecc9ce62
Marcos Valera <marvalpra1@alum.us.es>
2026-09-24 16:29:01 +0200

    fix: parte entera en viaje_ej2.py

--- src/viaje_ej2.py
+++ src/viaje_ej2.py
@@ -1,5 +1,7 @@
 distancia_km = int(input("Introduce la distancia en kilómetros >>> "))
 velocidad_kmh = int(input("Introduce la velocidad en kilómetros/hora >>> "))
-tiempo_horas = distancia_km // velocidad_kmh
-tiempo_dias = tiempo_horas // 24
+
+tiempo_horas = distancia_km / velocidad_kmh
+tiempo_dias = tiempo_horas / 24
+
 print(f"Tardarías {tiempo_dias} días en llegar.")
